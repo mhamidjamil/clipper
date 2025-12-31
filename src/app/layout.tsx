4808239d23edee8e9785d284dec9ac02e10a74f1
hamidjamil0420 <stewedchair@gmail.com>
2025-12-31 07:27:07 +0000

    Apply a dark theme UI design to the Barber–Client web application.
    Use

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,8 +32,8 @@ export default function RootLayout({
       <body className="font-body antialiased">
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="dark"
+          enableSystem={false}
           disableTransitionOnChange
         >
           <FirebaseClientProvider>
